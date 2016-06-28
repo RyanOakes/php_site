82f7870b6c3c37265d074a3c8104b62b7323ec36
oakes <oakesonline@gmail.com>
2016-06-28 18:53:29 -0400

    Creating inc assets for PHP

--- index.php
+++ index.php
@@ -1,29 +1,4 @@
-
-<html>
-<head>
-	<title><?php echo "Personal Media Library"; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="#">Books</a></li>
-                <li class="movies"><a href="#">Movies</a></li>
-                <li class="music"><a href="#">Music</a></li>
-                <li class="suggest"><a href="#">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
+<?php include("inc/header.php");?>
 		<div class="section catalog random">
 
 			<div class="wrapper">
@@ -38,22 +13,4 @@
 
 		</div>
 
-	</div>
-
-	<div class="footer">
-
-		<div class="wrapper">
-
-			<ul>
-				<li><a href="http://twitter.com/treehouse">Twitter</a></li>
-				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
-			</ul>
-
-			<p>&copy;<?php echo date("Y"); ?> Personal Media Library</p>
-
-		</div>
-
-	</div>
-
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
